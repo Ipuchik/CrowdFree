6d70b94c256cc5779e592e2bb9178b8fd4bc14ae
AM4SO <79862724+AM4SO@users.noreply.github.com>
2024-11-03 00:47:46 +0000

    update

--- connectToCloudVM.bat
+++ connectToCloudVM.bat
@@ -1,1 +1,1 @@
-ssh -i "key.pem" ec2-user@ec2-35-179-130-110.eu-west-2.compute.amazonaws.com
+ssh -i "key.pem" ec2-user@ec2-18-132-37-221.eu-west-2.compute.amazonaws.com
--- connectToCloudVM.sh
+++
@@ -1,1 +0,0 @@
-ssh -i "key.pem" ec2-user@ec2-35-179-130-110.eu-west-2.compute.amazonaws.com
